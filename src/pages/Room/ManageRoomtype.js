54f9510587cecdb7bb1421664a4c2f203a4bf191
suphamas <suphamas572@gmail.com>
2023-04-08 22:13:58 +0700

    add roomtype

--- src/pages/Room/ManageRoomtype.js
+++ src/pages/Room/ManageRoomtype.js
@@ -1,8 +1,65 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Button, Modal, Table, Input, Form, Select} from "antd";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import axios from "axios";
 
 const ManageRoomtpye=()=>{
+  const [dataOrg, setDataOrg] = useState([]);
+  function getOrg() {
+    axios.get("/org", { crossdomain: true }).then((response) => {
+      console.log(response);
+      setDataOrg(response.data);
+    });
+  }
+  const [dataSource, setDataSource] = useState([]);
+  function getRoomtpye(id) {
+    axios.get("/org/roomtype/"+id, { crossdomain: true }).then((response) => {
+      console.log(response);
+      setDataSource(response.data);
+    });
+  }
+  const [formData, setFormData] = useState({
+    name: "",
+    org:"",
+  });
+  const [idOrg, setIdorg] = useState();
+  const onChangeorg = (value) => {
+    console.log(`selected ${value}`);
+    setFormData({ ...formData, org: value})
+    setIdorg(value);
+    getRoomtpye(value)
+  };
+  const handleSubmit = () => {
+    console.log(formData)
+    axios
+      .post("/rooms/roomtype", formData)
+      .then((res) => {
+        getRoomtpye(idOrg);
+      })
+      .catch((err) => console.log(err));
+    setIsModalOpen(false);
+  };
+  useEffect(() => {
+    getOrg();
+  }, []);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const [isModalOpen2, setIsModalOpen2] = useState(false);
+  const handleCancel2 = () => {
+    setIsModalOpen2(false);
+  };
+  const showModal2 = () => {
+    setIsModalOpen2(true);
+  };
+
+  const handleOk2 = () => {
+    setIsModalOpen2(false);
+  };
     const [isEditingroomtype, setIsEditingroomtype] = useState(false);
     const [editingdataRoomtype, setEditingdataRoomtype] = useState(null);
     const [datastatusSource, setDatastatusSource] = useState([
@@ -23,7 +80,7 @@ const ManageRoomtpye=()=>{
         {
           key: "1",
           title: "Roomtype",
-          dataIndex: "roomtype",
+          dataIndex: "name",
         },
         {
           key: "2",
@@ -79,21 +136,68 @@ const ManageRoomtpye=()=>{
         const newUsers = {
          id: randomNumber,
          roomtype: 'Name',
-        }; 
+        };
         setDatastatusSource((pre) => {
           return [...pre, newUsers];
         });
       };
     return(
-        <div>
+      <React.Fragment>
+         <button
+            className="button-room"
+            type="primary"
+            size={20}
+            onClick={showModal2}
+          >
+            ManageRoomtpye
+          </button>
+          <div className="managestatus">
+            <Modal
+              title="ManageRoomtpye"
+              open={isModalOpen2}
+              onOk={handleOk2}
+              onCancel={handleCancel2}
+              footer={[]}
+            >
+
             <div className="User-list">
             <button className="button-submit1"
                   key="submit"
                   type="primary"
                   loading={loading}
-                  onClick={onAddUser}>AddRoomtype</button>
+                  onClick={showModal}>
+                    AddRoomtype
+                    </button>
+                  <Form.Item label="หน่วยงาน">
+        <Select
+          showSearch
+          placeholder="หน่วยงาน"
+          optionFilterProp="children"
+          onChange={onChangeorg}
+          filterOption={(input, option) =>
+            (option?.name ?? "").toLowerCase().includes(input.toLowerCase())
+          }
+          fieldNames={
+            { label: "name", value: "_id" }
+          }
+          options={dataOrg}
+        />
+      </Form.Item>
+      <Modal
+          title="AddRoomtype"
+          open={isModalOpen}
+          onOk={handleSubmit}
+          onCancel={handleCancel}
+        >
+          <Input
+            placeholder="AddRoomtype"
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            value={formData.name}
+          />
+        </Modal>
+
         <header className="User-list-heard">
-          <Table columns={columnsEdit} dataSource={datastatusSource}  pagination={false}></Table>
+          <Table columns={columnsEdit} dataSource={dataSource}  pagination={false}></Table>
           <Modal
             title="EditRoomtype"
             visible={isEditingroomtype}
@@ -125,7 +229,9 @@ const ManageRoomtpye=()=>{
           </Modal>
         </header>
         </div>
-          </div>
+        </Modal>
+              </div>
+          </React.Fragment>
     );
 }
 export default ManageRoomtpye;
