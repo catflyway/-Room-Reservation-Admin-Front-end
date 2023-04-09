356cec0b6bb655ff2a39118d1f57f65f78f7a426
suphamas <suphamas572@gmail.com>
2023-04-09 18:08:17 +0700

    addorgandsize

--- src/pages/Build/AddOrg.js
+++ src/pages/Build/AddOrg.js
@@ -1,102 +1,97 @@
-import {
-    Form,
-    Input,message,Button,Modal
-  } from 'antd';
-  import React, { useState } from 'react';
-  import axios from "axios";
-  
+import { Form, Input, message, Button, Modal } from "antd";
+import React, { useState } from "react";
+import axios from "axios";
 
-  const AddOrganization = ({onSuccess}) => {
-    const [form] = Form.useForm();
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const showModal = () => {
-      form.resetFields();
-      setLoading(false);
-      setIsModalOpen(true);
-    };
-  
-    const onCancelAdd = () => {
-      setIsModalOpen(false);
-    };
-    const onAddUser = (e) => {
-      form.submit();
-    };
-    const [loading, setLoading] = useState(false);
-    const onFormFinish = (value) => {
-      console.log("finish", value);
-      setLoading(true);
-      axios
-        .post("/org", value)
-        .then((response) => {
-          console.log("res", response);
-          setLoading(false);
-          setIsModalOpen(false);
-          message.success("เพิ่มหน่วยงานสำเร็จ");
-          if (typeof onSuccess === 'function') {
-            onSuccess();
-          }
-        })
-        .catch((err) => {
-          console.log("err", err);
-          setLoading(false);
-          message.error("ERROR");
-        });
-    };
-  
-    return (
-      <React.Fragment>
+const AddOrganization = ({ onSuccess }) => {
+  const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    form.resetFields();
+    setLoading(false);
+    setIsModalOpen(true);
+  };
+
+  const onCancelAdd = () => {
+    setIsModalOpen(false);
+  };
+  const onAddUser = (e) => {
+    form.submit();
+  };
+  const [loading, setLoading] = useState(false);
+  const onFormFinish = (value) => {
+    console.log("finish", value);
+    setLoading(true);
+    axios
+      .post("/org", value)
+      .then((response) => {
+        console.log("res", response);
+        setLoading(false);
+        setIsModalOpen(false);
+        message.success("เพิ่มหน่วยงานสำเร็จ");
+        if (typeof onSuccess === "function") {
+          onSuccess();
+        }
+      })
+      .catch((err) => {
+        console.log("err", err);
+        setLoading(false);
+        message.error("ERROR");
+      });
+  };
+
+  return (
+    <React.Fragment>
       <button
-            className="button-user"
-            type="primary"
-            size={20}
-            onClick={showModal}
-          >
-            AddOrganization
-          </button>
-<Modal
-title='AddOrganization'
-  open={isModalOpen}
-  onCancel={onCancelAdd}
-  footer={[
-    <Button className="button-back" key="back" onClick={onCancelAdd}>
-      ยกเลิก
-    </Button>,
+        className="button-user"
+        type="primary"
+        size={20}
+        onClick={showModal}
+      >
+        AddOrganization
+      </button>
+      <Modal
+        title="AddOrganization"
+        open={isModalOpen}
+        onCancel={onCancelAdd}
+        footer={[
+          <Button className="button-back" key="back" onClick={onCancelAdd}>
+            ยกเลิก
+          </Button>,
 
-    <Button
-      className="button-submit"
-      key="submit"
-      type="primary"
-      loading={loading}
-      onClick={onAddUser}
-    >
-      ตกลง
-    </Button>,
-  ]}
->
-      <Form
-      form={form}
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 14,
-        }}
-        layout="horizontal"
+          <Button
+            className="button-submit"
+            key="submit"
+            type="primary"
+            loading={loading}
+            onClick={onAddUser}
+          >
+            ตกลง
+          </Button>,
+        ]}
+      >
+        <Form
+          form={form}
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 14,
+          }}
+          layout="horizontal"
           onFinish={onFormFinish}
           disabled={loading}
-      >
-        <Form.Item label="Organization Name"
-        name="name"
-            rules={[{ min: 6, max: 25, required: true, whitespace: true }]}>
-          <Input placeholder='Organization Name'/>
-        </Form.Item>
-
-        
-      </Form>
+        >
+          <Form.Item
+            label="Organization Name"
+            name="name"
+            rules={[{ min: 6, max: 25, required: true, whitespace: true }]}
+          >
+            <Input placeholder="Organization Name" />
+          </Form.Item>
+        </Form>
       </Modal>
-      </React.Fragment>
-      
-    ); 
-  };
-  
-  export default AddOrganization;
+    </React.Fragment>
+  );
+};
+
+export default AddOrganization;
--- src/pages/Build/ManageOrganization.js
+++ src/pages/Build/ManageOrganization.js
@@ -1,48 +1,39 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import AddOrganization from "./AddOrg";
-import { Button, Modal, Table, Input, Form, Select } from "antd";
+import { Button, Modal, Table, Input, Form, Row,Col, Typography } from "antd";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import axios from "axios";
 
-import axios from 'axios';
-
-
-
-const { Option } = Select;
-const handleChange = (value) => {
-  console.log(`selected ${value}`);
-};
+const { Title } = Typography;
 
 const ManageOrganization = () => {
-  const [componentSize, setComponentSize] = useState('default');
-  
-    const onFormLayoutChange = ({ size }) => {
-      setComponentSize(size);
-    };
   const [isEditing, setIsEditing] = useState(false);
   const [editingStudent, setEditingStudent] = useState(null);
-  
+
   const [OrgID, setOrgID] = useState([]);
-      function getOrg(){
-        axios.get('/org',{crossdomain:true})
-        .then(response=>{
-          console.log(response)
-          setOrgID(response.data.map((item)=>{
-            return {...item,member:item.userID.length}  
-          }));
+  function getOrg() {
+    axios.get("/org", { crossdomain: true }).then((response) => {
+      console.log(response);
+      setOrgID(
+        response.data.map((item) => {
+          return { ...item, member: item.userID.length };
         })
-      }
-      const [UserContributorOrg, setUserContributorOrg] = useState([]);
-      function getUserContributorOrg(id){
-        axios.get('users/searchby?OrgID='+id,{crossdomain:true})
-        .then(response=>{
-          console.log(response)
-          setUserContributorOrg(response.data);
-        })
-      }
-      useEffect(() => {
-        getOrg();
-       }, []); 
-       
+      );
+    });
+  }
+  const [UserContributorOrg, setUserContributorOrg] = useState([]);
+  function getUserContributorOrg(id) {
+    axios
+      .get("users/searchby?OrgID=" + id, { crossdomain: true })
+      .then((response) => {
+        console.log(response);
+        setUserContributorOrg(response.data);
+      });
+  }
+  useEffect(() => {
+    getOrg();
+  }, []);
+
   const columns = [
     // {
     //   key: "1",
@@ -82,7 +73,6 @@ const ManageOrganization = () => {
       },
     },
   ];
-  const [isEditingstatus, setIsEditingstatus] = useState(false);
   const onDeleteStudent = (record) => {
     Modal.confirm({
       title: "Are you sure, you want to delete this organization record?",
@@ -103,43 +93,25 @@ const ManageOrganization = () => {
     setIsEditing(false);
     setEditingStudent(null);
   };
-  const onEditStatus = (record1) => {
-    setIsEditingstatus(true);
-    setEditingStudent({ ...record1 });
-  };
-  const resetEditingstatus = () => {
-    setIsEditingstatus(false);
-    setEditingStudent(null);
-  };
-
-  const [isAddOpen, setIsAddOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const showAdd = () => {
-    setIsAddOpen(true);
-  };
-
-  const handleCancelAdd = () => {
-    setIsAddOpen(false);
-  };
-
-  const handleCancel = () => {
-    setIsAddOpen(false);
-  };
   return (
     <div>
-      <div className="Heard-ManageUser">
-        <h5>ManageOrganization</h5>
-        <div className="button-manageorganization">
-          <AddOrganization  onSuccess={getOrg}/>
-        </div>
-      </div>
+      <Row justify="space-between" align="middle">
+      <Col>
+        <Title style={{ color: " #3F478D" }}>ManageOrganization</Title>
+      </Col>
+
+       <Col>
+          <AddOrganization onSuccess={getOrg} />
+        </Col>
+      </Row>
+
+
       <div className="User-list">
         <header className="User-list-heard">
           <Table columns={columns} dataSource={OrgID}></Table>
           <Modal
             title="Edit Organization"
-            visible={isEditing}
+            open={isEditing}
             okText="Save"
             onCancel={() => {
               resetEditing();
@@ -157,51 +129,46 @@ const ManageOrganization = () => {
               resetEditing();
             }}
           >
-          <Form
-      
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 14,
-      }}
-      layout="horizontal"
-      initialValues={{
-        size: componentSize,
-      }}
-      onValuesChange={onFormLayoutChange}
-      size={componentSize}
-    >
-          <Form.Item label='Organization Name'>
-            <Input
-              value={editingStudent?.namebuilding}
-              onChange={(e) => {
-                setEditingStudent((pre) => {
-                  return { ...pre, namebuilding: e.target.value };
-                });
+            <Form
+              labelCol={{
+                span: 8,
               }}
-            />
-            </Form.Item>
-            <Form.Item label="E-mail Contributor">
-            <Input
-              value={editingStudent?.email}
-              onChange={(e) => {
-                setEditingStudent((pre) => {
-                  return { ...pre, emai: e.target.value };
-                });
+              wrapperCol={{
+                span: 14,
               }}
-            />
-            </Form.Item>
-            <Form.Item label="Password Contributor">
-        <Input.Password value={editingStudent?.pass}
-              onChange={(e) => {
-                setEditingStudent((pre) => {
-                  return { ...pre, pass: e.target.value };
-                });
-              }}/>
-        </Form.Item>
-        </Form>
-            
+              layout="horizontal"
+            >
+              <Form.Item label="Organization Name">
+                <Input
+                  value={editingStudent?.namebuilding}
+                  onChange={(e) => {
+                    setEditingStudent((pre) => {
+                      return { ...pre, namebuilding: e.target.value };
+                    });
+                  }}
+                />
+              </Form.Item>
+              <Form.Item label="E-mail Contributor">
+                <Input
+                  value={editingStudent?.email}
+                  onChange={(e) => {
+                    setEditingStudent((pre) => {
+                      return { ...pre, emai: e.target.value };
+                    });
+                  }}
+                />
+              </Form.Item>
+              <Form.Item label="Password Contributor">
+                <Input.Password
+                  value={editingStudent?.pass}
+                  onChange={(e) => {
+                    setEditingStudent((pre) => {
+                      return { ...pre, pass: e.target.value };
+                    });
+                  }}
+                />
+              </Form.Item>
+            </Form>
           </Modal>
         </header>
       </div>
