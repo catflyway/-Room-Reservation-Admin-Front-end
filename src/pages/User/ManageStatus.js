0b481216386fc8a37a2e1217dcbdd68761495858
suphamas <suphamas572@gmail.com>
2023-04-22 18:40:24 +0700

    ย้าย ManageStatus

---
+++ src/pages/User/ManageStatus.js
@@ -0,0 +1,246 @@
+import React, { useState, useContext, Fragment } from "react";
+import {
+  Modal,
+  Table,
+  Input,
+  Form,
+  Select,
+  Row,
+  Col,
+  Space,
+  Button,
+} from "antd";
+import { EditOutlined, DeleteOutlined, PlusOutlined } from "@ant-design/icons";
+import axios from "axios";
+import { UserContext } from "../../user-context";
+
+const ManageStatus = ({ onChange = () => {}, orgList = [] }) => {
+  const user = useContext(UserContext);
+  const [orgForm] = Form.useForm();
+  const defaultOrg = user.canNotChangeOrg ? user.org.id : orgList[0]?._id;
+
+  const [statusList, setStatusList] = useState([]);
+  function getStatus(id) {
+    if (id) {
+      axios.get("/org/status/" + id).then((response) => {
+        setStatusList(response.data);
+      });
+    } else {
+      setStatusList([]);
+    }
+  }
+
+  const [isManageStatusOpen, setManageStatusOpen] = useState(false);
+  function openManageStatus() {
+    setManageStatusOpen(true);
+    orgForm.resetFields();
+    getStatus(defaultOrg);
+  }
+
+  function onChangeStatus() {
+    const orgId = orgForm.getFieldValue("org");
+    onChange(orgId);
+    getStatus(orgId);
+  }
+
+  /********** Add **********/
+  const [AddStatusForm] = Form.useForm();
+  const [isOpenAddStatus, setOpenAddStatus] = useState(false);
+  const openAddStatus = () => {
+    AddStatusForm.resetFields();
+    setOpenAddStatus(true);
+  };
+
+  const onAddStatusFinish = (formData) => {
+    const data = {
+      ...formData,
+      org: orgForm.getFieldValue("org"),
+    };
+    axios
+      .post("/users/status", data)
+      .then((res) => {
+        onChangeStatus();
+        setOpenAddStatus(false);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  /********** Edit **********/
+  const [editForm] = Form.useForm();
+  const [editingDatastatus, setEditingDatastatus] = useState(null);
+  const [isEditingstatus, setIsEditingstatus] = useState(false);
+  const [isEditingLoading, setIsEditingLoading] = useState(false);
+  const onEditstatus = (record) => {
+    setIsEditingstatus(true);
+    setEditingDatastatus(record);
+    editForm.setFieldsValue(record);
+  };
+  const onCancelEditingBuild = () => {
+    setIsEditingstatus(false);
+    setEditingDatastatus(null);
+  };
+  const onEditFinish = (formData) => {
+    setIsEditingLoading(true);
+    axios
+      .put(`/users/status/${editingDatastatus._id}`, formData)
+      .then((res) => {
+        onChangeStatus();
+
+        setIsEditingLoading(false);
+        setIsEditingstatus(false);
+      })
+      .catch((err) => {
+        console.log("/users/status/", err);
+        setIsEditingLoading(false);
+      });
+  };
+
+  /********** Delete **********/
+  function deleteStatus(statusId) {
+    axios.delete(`/users/status/${statusId}`).then((res) => {
+      onChangeStatus();
+    });
+  }
+
+  const onDeleteStatus = (record) => {
+    Modal.confirm({
+      title: "Are you sure, you want to delete this status record?",
+      okText: "Yes",
+      okType: "danger",
+      onOk: () => {
+        deleteStatus(record._id);
+      },
+    });
+  };
+
+  const columnsstatus = [
+    {
+      key: "1",
+      title: "Status",
+      dataIndex: "name",
+    },
+    {
+      key: "2",
+      title: "Actions",
+      render: (record) => {
+        return (
+          <Space size={"middle"}>
+            <EditOutlined
+              onClick={() => onEditstatus(record)}
+              style={{ color: "blue" }}
+            />
+            <DeleteOutlined
+              onClick={() => onDeleteStatus(record)}
+              style={{ color: "red" }}
+            />
+          </Space>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Fragment>
+      <Button type="primary" size="large" onClick={openManageStatus}>
+        ManageStatus
+      </Button>
+
+      <Modal
+        title="ManageStatus"
+        open={isManageStatusOpen}
+        onCancel={() => setManageStatusOpen(false)}
+        footer={false}
+      >
+        <Form form={orgForm}>
+          <Form.Item name="org" label="หน่วยงาน" initialValue={defaultOrg}>
+            <Select
+              showSearch
+              placeholder="หน่วยงาน"
+              optionFilterProp="children"
+              onChange={getStatus}
+              filterOption={(input, option) =>
+                (option?.name ?? "").toLowerCase().includes(input.toLowerCase())
+              }
+              fieldNames={{ label: "name", value: "_id" }}
+              options={orgList}
+              disabled={user.canNotChangeOrg}
+            />
+          </Form.Item>
+        </Form>
+
+        <Row align={"end"}>
+          <Col>
+            <Button
+              className="button-submit1"
+              type="primary"
+              onClick={openAddStatus}
+            >
+              <span>
+                <PlusOutlined />
+                Add Status
+              </span>
+            </Button>
+          </Col>
+        </Row>
+
+        <Modal
+          title="Add Status"
+          open={isOpenAddStatus}
+          onOk={AddStatusForm.submit}
+          onCancel={() => setOpenAddStatus(false)}
+        >
+          <Form
+            labelCol={{
+              span: 4,
+            }}
+            wrapperCol={{
+              span: 14,
+            }}
+            layout="horizontal"
+            form={AddStatusForm}
+            onFinish={onAddStatusFinish}
+          >
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[{ required: true, whitespace: true }]}
+            >
+              <Input placeholder="Statusname" />
+            </Form.Item>
+          </Form>
+        </Modal>
+        <Modal
+          title="EditStatus"
+          open={isEditingstatus}
+          okText="Save"
+          onCancel={() => onCancelEditingBuild()}
+          onOk={editForm.submit}
+          okButtonProps={{ loading: isEditingLoading }}
+        >
+          <Form
+            form={editForm}
+            onFinish={onEditFinish}
+            disabled={isEditingLoading}
+          >
+            <Form.Item
+              name="name"
+              rules={[{ required: true, whitespace: true }]}
+            >
+              <Input placeholder="Status" />
+            </Form.Item>
+          </Form>
+        </Modal>
+
+        <br />
+        <Table
+          columns={columnsstatus}
+          dataSource={statusList}
+          pagination={false}
+          rowKey="_id"
+        ></Table>
+      </Modal>
+    </Fragment>
+  );
+};
+
+export default ManageStatus;
--- src/pages/User/ManageUser.js
+++ src/pages/User/ManageUser.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, Fragment } from "react";
 import {
   Modal,
   Table,
@@ -10,9 +10,9 @@ import {
   Col,
   Space,
   Typography,
-  Button,
 } from "antd";
 import EditUser from "./EditUser";
+import ManageStatus from "./ManageStatus";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import axios from "axios";
 import { UserContext } from "../../user-context";
@@ -22,64 +22,28 @@ const { Search } = Input;
 
 const ManageUser = () => {
   const user = useContext(UserContext);
-  const [form] = Form.useForm();
-  const [formData, setFormData] = useState({
-    name: "",
-    org: "",
-  });
+  const [filterForm] = Form.useForm();
+
+  /********** Get data from API **********/
   const [orgList, setOrgList] = useState([]);
   function getOrg() {
     axios.get("/org").then((response) => {
-      console.log("org", response.data);
       setOrgList(response.data);
     });
   }
-  const [dataUsers, setDataUsers] = useState([]);
-  const [status, setstatus] = useState([]);
-  function getStatus(id) {
-    axios.get("/org/status/" + id).then((response) => {
-      console.log("org", "status", id, response.data);
-      setstatus(response.data);
-    });
-  }
-  const canNotChangeOrg = ["Room Contributor", "Contributor"].includes(
-    user.role
-  );
-  let initialValues = {};
-  if (canNotChangeOrg) {
-    initialValues["org"] = user.org.id;
-  }
-  let userRoleOption = [
-    {
-      value: "User",
-      label: "User",
-    },
-    {
-      value: "Room Contributor",
-      label: "Room Contributor",
-    },
-    {
-      value: "Contributor",
-      label: "Contributor",
-    },
-  ];
-  if (user.role == "Administrator") {
-    userRoleOption.push({
-      value: "Administrator",
-      label: "Administrator",
-    });
-  }
-  useEffect(() => {
-    getOrg();
-    if (canNotChangeOrg) {
-      onChangeorg(user.org.id);
-      form.setFieldValue("org", user.org.id);
-      getManageUsers({ org: user.org.id });
-    } else {
-      getManageUsers();
-    }
-  }, []);
 
+  const [statusList, setStatusList] = useState([]);
+  function getStatus(id) {
+    if (id) {
+      axios.get("/org/status/" + id).then((response) => {
+        setStatusList(response.data);
+      });
+    } else {
+      setStatusList([]);
+    }
+  }
+
+  const [dataUsers, setDataUsers] = useState([]);
   function getManageUsers(option) {
     axios.get("/users/searchby", { params: option }).then((response) => {
       setDataUsers(
@@ -93,21 +57,64 @@ const ManageUser = () => {
       );
     });
   }
-  const [idOrg, setIdorg] = useState();
-  const onChangeorg = (value) => {
-    console.log(`selected ${value}`);
-    setFormData({ ...formData, org: value });
-    setIdorg(value);
-    if (value) {
-      getStatus(value);
-    }
-    form.resetFields(["status"]);
+
+  /********** Filter callback **********/
+  const onChangeOrg = (value) => {
+    getStatus(value);
+    filterForm.resetFields(["status"]);
   };
 
-  const [componentSize, setComponentSize] = useState("default");
+  const onFilterChange = (changedValues, allValues) => {
+    if (changedValues.hasOwnProperty("org")) {
+      onChangeOrg(changedValues.org);
+      allValues.status = undefined;
+      changedValues.status = undefined;
+    }
+    getManageUsers(allValues);
+  };
 
-  const onFormLayoutChange = ({ size }) => {
-    setComponentSize(size);
+  const [SearchUserList, setSearchUserList] = useState([]);
+  function getSearchuser(id) {
+    axios.get("/users/search/" + id).then((response) => {
+      setSearchUserList(response.data);
+    });
+  }
+  const [selectedItems, setSelectedItems] = useState([]);
+  const filteredOptions = SearchUserList.filter(
+    (o) => !selectedItems.includes(o)
+  );
+
+  function reloadUsers() {
+    getManageUsers(filterForm.getFieldsValue());
+  }
+
+  function reloadStatus() {
+    getStatus(filterForm.getFieldValue("org"));
+  }
+
+  /********** User table row action **********/
+  function deleteUser(id) {
+    axios.delete("/users/" + id).then((res) => {
+      reloadUsers();
+    });
+  }
+
+  const onDeleteUser = (record) => {
+    Modal.confirm({
+      title: "Are you sure, you want to delete this user record?",
+      okText: "Yes",
+      okType: "danger",
+      onOk: () => {
+        deleteUser(record._id);
+      },
+    });
+  };
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingData, setEditingdata] = useState(null);
+  const onEditUser = (record) => {
+    setIsEditing(true);
+    setEditingdata({ ...record });
   };
 
   const columns = [
@@ -159,15 +166,9 @@ const ManageUser = () => {
       render: (record) => {
         return (
           <>
-            <EditOutlined
-              onClick={() => {
-                onEditUser(record);
-              }}
-            />
+            <EditOutlined onClick={() => onEditUser(record)} />
             <DeleteOutlined
-              onClick={() => {
-                onDeleteUser(record);
-              }}
+              onClick={() => onDeleteUser(record)}
               style={{ color: "red", marginLeft: 12 }}
             />
           </>
@@ -177,178 +178,55 @@ const ManageUser = () => {
       align: "center",
     },
   ];
-  const columnsstatus = [
+
+  let userRoleOption = [
     {
-      key: "1",
-      title: "Status",
-      dataIndex: "name",
+      value: "User",
+      label: "User",
     },
     {
-      key: "2",
-      title: "Actions",
-      render: (record) => {
-        return (
-          <>
-            <EditOutlined
-              onClick={() => {
-                onEditstatus(record);
-              }}
-              style={{ color: "blue", marginLeft: 12 }}
-            />
-            <DeleteOutlined
-              onClick={() => {
-                onDeleteStatus(record);
-              }}
-              style={{ color: "red", marginLeft: 12 }}
-            />
-          </>
-        );
-      },
+      value: "Room Contributor",
+      label: "Room Contributor",
+    },
+    {
+      value: "Contributor",
+      label: "Contributor",
     },
   ];
-  function deleteUser(id) {
-    axios.delete("/users/" + id).then((res) => {
-      if (canNotChangeOrg) {
-        onChangeorg(user.org.id);
-        form.setFieldValue("org", user.org.id);
-        getManageUsers({ org: user.org.id });
-      } else {
-        getManageUsers();
-      }
+  if (!user.canNotChangeOrg) {
+    userRoleOption.push({
+      value: "Administrator",
+      label: "Administrator",
     });
   }
 
-  const onDeleteUser = (record) => {
-    Modal.confirm({
-      title: "Are you sure, you want to delete this user record?",
-      okText: "Yes",
-      okType: "danger",
-      onOk: () => {
-        deleteUser(record._id);
-      },
-    });
-  };
-  const onDeleteStatus = (record) => {
-    Modal.confirm({
-      title: "Are you sure, you want to delete this status record?",
-      okText: "Yes",
-      okType: "danger",
-      onOk: () => {
-        deleteStatus(record._id);
-      },
-    });
-  };
-  const [isEditing, setIsEditing] = useState(false);
-  const [editingData, setEditingdata] = useState(null);
-  const onEditUser = (record) => {
-    setIsEditing(true);
-    setEditingdata({ ...record });
-  };
-  const [editForm] = Form.useForm();
-  const [editingDatastatus, setEditingDatastatus] = useState(null);
-  const [isEditingstatus, setIsEditingstatus] = useState(false);
-  const [isEditingLoading, setIsEditingLoading] = useState(false);
-  const onEditstatus = (record) => {
-    console.log("edit data", record);
-    setIsEditingstatus(true);
-    setEditingDatastatus(record);
-    editForm.setFieldsValue(record);
-  };
-  const onCancelEditingBuild = () => {
-    setIsEditingstatus(false);
-    setEditingDatastatus(null);
-  };
-  const onEditFinish = (formData) => {
-    console.log(formData, editingDatastatus);
-
-    setIsEditingLoading(true);
-    axios
-      .put("/users/status/" + editingDatastatus._id, formData)
-      .then((res) => {
-        console.log("/users/status/", res.data);
-        getStatus(idOrg);
-        setIsEditingLoading(false);
-        setIsEditingstatus(false);
-      })
-      .catch((err) => {
-        console.log("/users/status/", err);
-        setIsEditingLoading(false);
-      });
-  };
-
-  const [isModalOpen1, setIsModalOpen1] = useState(false);
-  const [AddStatusU, setIsAddStatusUser] = useState(false);
-
-  const openaddstatus = () => {
-    setIsAddStatusUser(true);
-  };
-  const Canceladdstatus = () => {
-    setIsAddStatusUser(false);
-  };
-
-  const showModal1 = () => {
-    setIsModalOpen1(true);
-  };
-
-  const handleOk1 = () => {
-    setIsModalOpen1(false);
-  };
-
-  const handleCancel1 = () => {
-    setIsModalOpen1(false);
-  };
-
-  const handleSubmit = () => {
-    console.log(formData);
-    axios
-      .post("/users/status", formData)
-      .then((res) => {
-        getStatus(idOrg);
-      })
-      .catch((err) => console.log(err));
-    setIsAddStatusUser(false);
-  };
-  function deleteStatus() {
-    axios.delete("/users/status").then((res) => {
-      if (canNotChangeOrg) {
-        onChangeorg(user.org.id);
-        form.setFieldValue("org", user.org.id);
-        getStatus({ org: user.org.id });
-      } else {
-        getStatus();
-      }
-    });
-  }
-  const onChangestatus = (buildingID) => {
-    console.log(`selected ${buildingID}`);
-  };
-
-  const [SearchUserList, setSearchUserList] = useState([]);
-  function getSearchuser(id) {
-    axios.get("/users/search/" + id).then((response) => {
-      setSearchUserList(response.data);
-    });
-  }
-  const [selectedItems, setSelectedItems] = useState([]);
-  const filteredOptions = SearchUserList.filter(
-    (o) => !selectedItems.includes(o)
-  );
-  const onFilterChange = (changedValues, allValues) => {
-    console.log(changedValues, allValues);
-    getManageUsers(allValues);
-  };
+  useEffect(() => {
+    getOrg();
+    if (user.canNotChangeOrg) {
+      onChangeOrg(user.org.id);
+      getManageUsers({ org: user.org.id });
+    } else {
+      getManageUsers();
+    }
+  }, []);
 
   return (
-    <div className="ManageUser">
+    <Fragment>
       <Row justify="space-between" align="middle">
         <Col>
           <Title style={{ color: " #3F478D" }}>ManageUser</Title>
         </Col>
         <Col>
           <Space wrap>
-            <Button type="primary" size="large" onClick={showModal1}>
-              ManageStatus
-            </Button>
+            <ManageStatus
+              onChange={(orgId) => {
+                if (orgId !== filterForm.getFieldValue("org")) {
+                  return;
+                }
+                reloadStatus();
+              }}
+              orgList={orgList}
+            />
             <EditUser
               value={editingData}
               openEdit={isEditing}
@@ -357,7 +235,7 @@ const ManageUser = () => {
               }}
               onSuccess={() => {
                 setIsEditing(false);
-                getManageUsers();
+                reloadUsers();
               }}
             />
           </Space>
@@ -366,9 +244,13 @@ const ManageUser = () => {
 
       <br />
       <Row justify="center" gutter={[16, 16]}>
-        <Form form={form} onValuesChange={onFilterChange}>
+        <Form form={filterForm} onValuesChange={onFilterChange}>
           <Space wrap>
-            <Form.Item label="Organization" name="org">
+            <Form.Item
+              label="Organization"
+              name="org"
+              initialValue={user.canNotChangeOrg ? undefined : user.org.id}
+            >
               <Select
                 style={{
                   width: "200px",
@@ -376,8 +258,7 @@ const ManageUser = () => {
                 allowClear
                 showSearch
                 placeholder="Organization"
-                optionFilterProp="children"
-                onChange={onChangeorg}
+                optionFilterProp="label"
                 filterOption={(input, option) =>
                   (option?.name ?? "")
                     .toLowerCase()
@@ -385,7 +266,7 @@ const ManageUser = () => {
                 }
                 fieldNames={{ label: "name", value: "_id" }}
                 options={orgList}
-                disabled={canNotChangeOrg}
+                disabled={user.canNotChangeOrg}
               />
             </Form.Item>
 
@@ -395,17 +276,16 @@ const ManageUser = () => {
                   width: "200px",
                 }}
                 allowClear
-                showSearch
+                // showSearch
                 placeholder="Status"
-                optionFilterProp="children"
-                onChange={onChangestatus}
+                optionFilterProp="label"
                 filterOption={(input, option) =>
                   (option?.name ?? "")
                     .toLowerCase()
                     .includes(input.toLowerCase())
                 }
                 fieldNames={{ label: "name", value: "_id" }}
-                options={status}
+                options={statusList}
               />
             </Form.Item>
 
@@ -449,104 +329,7 @@ const ManageUser = () => {
 
       <br />
       <Table columns={columns} dataSource={dataUsers} rowKey="_id"></Table>
-
-      <Modal
-        title="ManageStatus"
-        open={isModalOpen1}
-        onOk={handleOk1}
-        onCancel={handleCancel1}
-        footer={[]}
-      >
-        <button
-          className="button-submit1"
-          id="1"
-          type="primary"
-          onClick={openaddstatus}
-        >
-          AddStatus
-        </button>
-        <Form initialValues={initialValues}>
-          <Form.Item name="org" label="หน่วยงาน">
-            <Select
-              showSearch
-              placeholder="หน่วยงาน"
-              optionFilterProp="children"
-              onChange={onChangeorg}
-              filterOption={(input, option) =>
-                (option?.name ?? "").toLowerCase().includes(input.toLowerCase())
-              }
-              fieldNames={{ label: "name", value: "_id" }}
-              options={orgList}
-              disabled={canNotChangeOrg}
-            />
-          </Form.Item>
-        </Form>
-        <Modal
-          title="AddStatus"
-          open={AddStatusU}
-          onOk={handleSubmit}
-          onCancel={Canceladdstatus}
-        >
-          <Form
-            labelCol={{
-              span: 4,
-            }}
-            wrapperCol={{
-              span: 14,
-            }}
-            layout="horizontal"
-            initialValues={{
-              size: componentSize,
-            }}
-            onValuesChange={onFormLayoutChange}
-            size={componentSize}
-          >
-            <Form.Item label="Statusname">
-              <Input
-                placeholder="Statusname"
-                onChange={(e) =>
-                  setFormData({ ...formData, name: e.target.value })
-                }
-                value={formData.name}
-              />
-            </Form.Item>
-          </Form>
-        </Modal>
-        <Modal
-          title="EditStatus"
-          open={isEditingstatus}
-          okText="Save"
-          onCancel={() => {
-            onCancelEditingBuild();
-          }}
-          onOk={() => {
-            editForm.submit();
-          }}
-          okButtonProps={{ loading: isEditingLoading }}
-        >
-          <Form
-            form={editForm}
-            onFinish={onEditFinish}
-            disabled={isEditingLoading}
-          >
-            <Form.Item
-              name="name"
-              rules={[{ required: true, whitespace: true }]}
-            >
-              <Input placeholder="Status" />
-            </Form.Item>
-          </Form>
-        </Modal>
-        <header className="User-list-heard">
-          <Table
-            columns={columnsstatus}
-            dataSource={status}
-            pagination={false}
-            rowKey="_id"
-          ></Table>
-        </header>
-      </Modal>
-    </div>
+    </Fragment>
   );
 };
 
